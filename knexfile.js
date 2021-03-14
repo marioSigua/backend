8eefe02d1fba54179984586d745d1b7654307b0f
cliljdn <caliljaudiannn@gmail.com>
2021-03-14 20:17:10 +0800

    wip -> knex migrations

--- knexfile.js
+++ knexfile.js
@@ -1,33 +1,34 @@
 // Update with your config settings.
-
+//connection ng nodejs sa xampp yung client yan yung gagamitin na db
+// host ilagay nyo sa .env yung variable na DB_HOST dapat yan localhost or depende sa port ng gamit niyo
+// properties na migrations jan pupunta yung ggawin niyo na migrations yung migration table yon
+// yung seed test data yan lalgyan nya ng mga laman yung table.
 module.exports = {
-
   development: {
-      client: 'mysql',
-      connection: {
-        host: process.env.DB_HOST,
-        database: 'thesisdb',
-        user: 'root',
-        pass: '',
-        // user: 'cliljdn',
-        // password: 'jaudian29',
-        timezone: 'UTC',
-        dateStrings: true,
-      },
-      
-      pool: {
-        min: 2,
-        max: 10,
-      },
-    
-		migrations: {
-			directory: './database/migrations',
-    },
-    
-		seeds: {
-			directory: './database/seeds',
-  
-    },
-	},
+    client: 'mysql',
 
-};
+    connection: {
+      host: process.env.DB_HOST,
+      database: 'thesisdb',
+      user: 'root',
+      pass: '',
+      // user: 'cliljdn',
+      // password: 'jaudian29',
+      timezone: 'UTC',
+      dateStrings: true,
+    },
+
+    pool: {
+      min: 2,
+      max: 10,
+    },
+
+    migrations: {
+      directory: './database/migrations',
+    },
+
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
+}
