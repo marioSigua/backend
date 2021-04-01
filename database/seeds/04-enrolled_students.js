44073c244a3de3a4bd1973ef0b4d21de0ed82355
cliljdn <caliljaudiannn@gmail.com>
2021-04-02 04:31:10 +0800

    wip -> added req for enroll students

--- database/seeds/04-enrolled_students.js
+++ database/seeds/04-enrolled_students.js
@@ -10,49 +10,49 @@ exports.seed = function (knex) {
                         {
                               student_id: 1,
                               subject_code: 'CMPE563',
-                              current_sem: '1st Sem',
+                              current_sem: '2nd SEMESTER',
                               school_year: '2020-2021',
                         },
 
                         {
                               student_id: 2,
                               subject_code: 'CMPE563',
-                              current_sem: '1st Sem',
+                              current_sem: '2nd SEMESTER',
                               school_year: '2020-2021',
                         },
 
                         {
                               student_id: 3,
                               subject_code: 'CMPE563',
-                              current_sem: '1st Sem',
+                              current_sem: '2nd SEMESTER',
                               school_year: '2020-2021',
                         },
 
                         {
                               student_id: 4,
                               subject_code: 'CMPE563',
-                              current_sem: '1st Sem',
+                              current_sem: '2nd SEMESTER',
                               school_year: '2020-2021',
                         },
 
                         {
                               student_id: 5,
                               subject_code: 'CMPE563',
-                              current_sem: '1st Sem',
+                              current_sem: '2nd SEMESTER',
                               school_year: '2020-2021',
                         },
 
                         {
                               student_id: 6,
                               subject_code: 'CMPE563',
-                              current_sem: '1st Sem',
+                              current_sem: '2nd SEMESTER',
                               school_year: '2020-2021',
                         },
 
                         {
                               student_id: 7,
                               subject_code: 'CMPE563',
-                              current_sem: '1st Sem',
+                              current_sem: '2nd SEMESTER',
                               school_year: '2020-2021',
                         },
                   ])
