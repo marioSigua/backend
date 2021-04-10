db6d5cc81c508be33514cb65fe265d89c1e4a9be
cliljdn <caliljaudiannn@gmail.com>
2021-04-11 00:11:10 +0800

    fix -> questions endpoint

--- database/models/subjects.model.js
+++ database/models/subjects.model.js
@@ -2,57 +2,62 @@ const tableNames = require('../../lib/constants/tablenames')
 const { Model } = require('objection')
 
 class SubjectModel extends Model {
-      static get tableName() {
-            return tableNames.subjects_tbl
-      }
+    static get tableName() {
+        return tableNames.subjects_tbl
+    }
 
-      static get idColumn() {
-            return 'subject_code'
-      }
+    static get idColumn() {
+        return 'subject_code'
+    }
 
-      static get relationMappings() {
-            const StudentsModel = require('./students.model')
-            const EnrolledStudents = require('./enrolled_students.model')
+    static get relationMappings() {
+        const StudentsModel = require('./students.model')
+        const EnrolledStudents = require('./enrolled_students.model')
+        const QuestionModel = require('./questions.model')
 
-            return {
-                  students: {
-                        relation: Model.ManyToManyRelation,
-                        modelClass: () => StudentsModel,
+        return {
+            students: {
+                relation: Model.ManyToManyRelation,
+                modelClass: () => StudentsModel,
 
-                        join: {
-                              from: this.tableName + '.subject_code',
-                              through: {
-                                    from:
-                                          tableNames.enrolled_subjects +
-                                          '.subject_code',
+                join: {
+                    from: this.tableName + '.subject_code',
+                    through: {
+                        from: tableNames.enrolled_subjects + '.subject_code',
 
-                                    to:
-                                          tableNames.enrolled_subjects +
-                                          '.student_id',
+                        to: tableNames.enrolled_subjects + '.student_id',
 
-                                    extra: [
-                                          'prelim_grade',
-                                          'midterm_grade',
-                                          'finals_grade',
-                                    ],
-                              },
-                              to: tableNames.students_tbl + '.student_id',
-                        },
-                  },
+                        extra: [
+                            'prelim_grade',
+                            'midterm_grade',
+                            'finals_grade',
+                        ],
+                    },
+                    to: tableNames.students_tbl + '.student_id',
+                },
+            },
 
-                  studentGrades: {
-                        relation: Model.HasManyRelation,
-                        modelClass: () => EnrolledStudents,
-                        join: {
-                              from: this.tableName + '.subject_code',
+            studentGrades: {
+                relation: Model.HasManyRelation,
+                modelClass: () => EnrolledStudents,
+                join: {
+                    from: this.tableName + '.subject_code',
 
-                              to:
-                                    tableNames.enrolled_subjects +
-                                    '.subject_code',
-                        },
-                  },
-            }
-      }
+                    to: tableNames.enrolled_subjects + '.subject_code',
+                },
+            },
+
+            questionTopics: {
+                relation: Model.HasManyRelation,
+                modelClass: () => QuestionModel,
+                join: {
+                    from: this.tableName + '.subject_code',
+
+                    to: tableNames.questions_tbl + '.subject_code',
+                },
+            },
+        }
+    }
 }
 
 module.exports = SubjectModel
