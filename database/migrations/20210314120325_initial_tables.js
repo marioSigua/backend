5ecd441b599ecbe06c2c0175e34fa2cc8a5d5827
cliljdn <caliljaudiannn@gmail.com>
2021-04-11 02:27:06 +0800

    pulled master

--- database/migrations/20210314120325_initial_tables.js
+++ database/migrations/20210314120325_initial_tables.js
@@ -5,114 +5,115 @@ const Knex = require('knex')
  * @param {Knex} knex
  */
 exports.up = async function (knex) {
-      await knex.schema.createTable(tableNames.accounts_tbl, function (table) {
-            table.increments('account_id').notNullable()
-            table.string('email')
-            table.string('password')
-            table.string('account_type')
-            table.string('firstname').nullable()
-            table.string('lastname').nullable()
-            table.timestamps(true, true)
-      })
+    await knex.schema.createTable(tableNames.accounts_tbl, function (table) {
+        table.increments('account_id').notNullable()
+        table.string('email')
+        table.string('password')
+        table.string('account_type')
+        table.string('firstname').nullable()
+        table.string('lastname').nullable()
+        table.timestamps(true, true)
+    })
 
-      await knex.schema.createTable(tableNames.students_tbl, function (table) {
-            table.string('student_id', 255).notNullable().primary()
-            table.string('student_email')
-            table.string('firstname')
-            table.string('lastname')
-            table.string('student_course')
-            table.boolean('isEnrolled').defaultTo(false)
-            table.timestamps(true, true)
-      })
+    await knex.schema.createTable(tableNames.students_tbl, function (table) {
+        table.string('student_id', 255).notNullable().primary()
+        table.string('student_email')
+        table.string('firstname')
+        table.string('lastname')
+        table.string('student_course')
+        table.boolean('isEnrolled').defaultTo(false)
+        table.timestamps(true, true)
+    })
 
-      await knex.schema.createTable(tableNames.subjects_tbl, function (table) {
-            table.string('subject_code', 255).notNullable().primary()
-            table.string('subject_name')
-            table.string('subject_desc')
-            table.string('subject_sem')
-            table.string('subject_year')
-            table.string('subject_course')
+    await knex.schema.createTable(tableNames.subjects_tbl, function (table) {
+        table.string('subject_code', 255).notNullable().primary()
+        table.string('subject_name')
+        table.string('subject_desc')
+        table.string('subject_sem')
+        table.string('subject_year')
+        table.string('subject_course')
 
-            //id ng prof
-            table.integer('account_id')
-                  .unsigned()
-                  .references('account_id')
-                  .inTable(tableNames.accounts_tbl)
-                  .onDelete('CASCADE')
-                  .index()
+        //id ng prof
+        table
+            .integer('account_id')
+            .unsigned()
+            .references('account_id')
+            .inTable(tableNames.accounts_tbl)
+            .onDelete('CASCADE')
+            .index()
 
-            table.boolean('isOccupied').defaultTo(false)
+        table.boolean('isOccupied').defaultTo(false)
 
-            table.string('current_year').defaultTo(new Date().getFullYear())
-            table.timestamps(true, true)
-      })
+        table.string('current_year').defaultTo(new Date().getFullYear())
+        table.timestamps(true, true)
+    })
 
-      await knex.schema.createTable(
-            tableNames.enrolled_subjects,
-            function (table) {
-                  table.increments('enrolled_id').notNullable()
+    await knex.schema.createTable(
+        tableNames.enrolled_subjects,
+        function (table) {
+            table.increments('enrolled_id').notNullable()
 
-                  table.float('prelim_grade')
-                  table.float('midterm_grade')
-                  table.float('finals_grade')
-                  table.string('current_sem')
-                  table.string('school_year')
+            table.float('prelim_grade')
+            table.float('midterm_grade')
+            table.float('finals_grade')
+            table.string('current_sem')
+            table.string('school_year')
 
-                  table.string('student_id', 255)
-                        .references('student_id')
-                        .inTable(tableNames.students_tbl)
-                        .index()
+            table
+                .string('student_id', 255)
+                .references('student_id')
+                .inTable(tableNames.students_tbl)
+                .index()
 
-                  table.string('subject_code', 255)
-                        .references('subject_code')
-                        .inTable(tableNames.subjects_tbl)
-                        .index()
-
-                  table.timestamps(true, true)
-            }
-      )
-
-      await knex.schema.createTable(tableNames.questions_tbl, function (table) {
-            table.increments('question_id').notNullable()
-
-            table.string('form_topic')
-
-            table.jsonb('question_form')
-
-            // table.string('student_id', 255)
-            //       .references('student_id')
-            //       .inTable(tableNames.students_tbl)
-            //       .index()
-
-            table.string('subject_code', 255)
-                  .references('subject_code')
-                  .inTable(tableNames.subjects_tbl)
-                  .index()
+            table
+                .string('subject_code', 255)
+                .references('subject_code')
+                .inTable(tableNames.subjects_tbl)
+                .index()
 
             table.timestamps(true, true)
-      })
+        }
+    )
 
-      await knex.schema.createTable(tableNames.response_tbl, function (table) {
-            table.increments('response_id').notNullable()
+    await knex.schema.createTable(tableNames.questions_tbl, function (table) {
+        table.increments('question_id').notNullable()
 
-            table.jsonb('student_reponse')
+        table.string('term')
 
-            table.integer('student_score')
+        table.jsonb('question_form')
 
-            table.string('student_id', 255)
-                  .references('student_id')
-                  .inTable(tableNames.students_tbl)
-                  .index()
+        table
+            .string('subject_code', 255)
+            .references('subject_code')
+            .inTable(tableNames.subjects_tbl)
+            .index()
 
-            table.integer('question_id')
-                  .unsigned()
-                  .references('question_id')
-                  .inTable(tableNames.questions_tbl)
-                  .onDelete('CASCADE')
-                  .index()
+        table.timestamps(true, true)
+    })
 
-            table.timestamps(true, true)
-      })
+    await knex.schema.createTable(tableNames.response_tbl, function (table) {
+        table.increments('response_id').notNullable()
+
+        table.jsonb('student_reponse')
+
+        table.integer('student_score')
+
+        table
+            .string('student_id', 255)
+            .references('student_id')
+            .inTable(tableNames.students_tbl)
+            .index()
+
+        table
+            .integer('question_id')
+            .unsigned()
+            .references('question_id')
+            .inTable(tableNames.questions_tbl)
+            .onDelete('CASCADE')
+            .index()
+
+        table.timestamps(true, true)
+    })
 }
 
 /**
@@ -120,10 +121,10 @@ exports.up = async function (knex) {
  * @param {Knex} knex
  */
 exports.down = async function (knex) {
-      await knex.schema.dropTableIfExists(tableNames.response_tbl)
-      await knex.schema.dropTableIfExists(tableNames.questions_tbl)
-      await knex.schema.dropTableIfExists(tableNames.enrolled_subjects)
-      await knex.schema.dropTableIfExists(tableNames.subjects_tbl)
-      await knex.schema.dropTableIfExists(tableNames.students_tbl)
-      await knex.schema.dropTableIfExists(tableNames.accounts_tbl)
+    await knex.schema.dropTableIfExists(tableNames.response_tbl)
+    await knex.schema.dropTableIfExists(tableNames.questions_tbl)
+    await knex.schema.dropTableIfExists(tableNames.enrolled_subjects)
+    await knex.schema.dropTableIfExists(tableNames.subjects_tbl)
+    await knex.schema.dropTableIfExists(tableNames.students_tbl)
+    await knex.schema.dropTableIfExists(tableNames.accounts_tbl)
 }
